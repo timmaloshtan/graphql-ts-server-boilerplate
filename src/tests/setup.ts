fd331dfea7c7680f697187391853c26c4e08e534
Tim M <tim917@gmail.com>
2020-03-24 09:24:48 +0200

    Refactor test setup

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,11 +1,12 @@
 import { startServer } from "../startServer";
 import { createTypeormConnection } from "../utils/createTypeormConnection";
+import { AddressInfo } from "net";
 
 module.exports = async () => {
   const globalAny: any = global;
 
   const app = await startServer();
-  const { port } = globalAny.__TEST_SERVER__.address();
+  const { port } = app.address() as AddressInfo;
   process.env.TEST_HOST = `http://127.0.0.1:${port}`;
 
   globalAny.__TEST_SERVER__ = app;
